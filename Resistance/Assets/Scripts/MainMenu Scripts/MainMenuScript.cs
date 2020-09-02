2377908d3f86980cc697bb3d14407a2f39d6d520
Scuftz <shivneel.singh1600@gmail.com>
2020-09-02 23:59:28 +1200

    Created Host/Join Lobby Ability
    - Can now create lobby or join a lobby
    - Also created a built version of current game to test localhost connection

--- Resistance/Assets/Scripts/MainMenu Scripts/MainMenuScript.cs
+++ Resistance/Assets/Scripts/MainMenu Scripts/MainMenuScript.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
     public void QuitGame()
     {
         Application.Quit();
