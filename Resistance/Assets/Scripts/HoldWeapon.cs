dee0bc091ae8c8038a30e862aa85cd8eec86cae0
EllairaT <EllairaT@users.noreply.github.com>
2020-09-07 05:18:50 +1200

    AnimationFix
    Fixed Animation for prefabs

---
+++ Resistance/Assets/Scripts/HoldWeapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldWeapon : MonoBehaviour
+{
+    public Transform leftBone;
+    public Transform leftReference;
+    public Transform rightBone;
+    public Transform rightReference;
+    public Transform elbowBone;
+    public Transform elbowReference;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftBone.transform.parent = leftReference.transform;
+        rightBone.transform.parent = rightReference.transform;
+        elbowBone.transform.parent = elbowReference.transform;
+        
+    }
+}
---
+++ Resistance/Assets/Scripts/StructureManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class StructureManager : MonoBehaviour
+{
+
+    public Structure s;
+    public void CreateStructure(GameObject spawnPoint)
+    {
+        s.GetStructure(spawnPoint);
+    }
+}
