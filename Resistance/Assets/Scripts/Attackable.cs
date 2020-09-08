6c7ae00b20b4672350f03f9689984e40dd065256
EllairaT <EllairaT@users.noreply.github.com>
2020-09-08 18:49:20 +1200

    Revert "Merge branch 'master' into lobby-system-branch"
    This reverts commit 7c7e05d82224f86cd05990258e8f90e52a57691a, reversing
    changes made to b3484f3869c0d5662375f8a92bf039931ccb9e9f.

--- Resistance/Assets/Scripts/Attackable.cs
+++ Resistance/Assets/Scripts/Attackable.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 public class Attackable : MonoBehaviour
 {
     public float health = 50f;
-    public bool isStructure;
-    public Structure structure;
-
-    void Start()
-    {
-        if (isStructure)
-        {
-            health = structure.baseHealth;
-        }
-    }
 
     public void TakeDamage(float amount)
     {
-        if (isStructure)
-        {
-            amount = structure. CalculateDamage(amount);
-        }
-
         health -= amount;
-        Debug.Log("ouch");
-
         if (health <= 0f)
         {
             Die();
         }
     }
-
     void Die ()
     {
         Destroy(gameObject);
