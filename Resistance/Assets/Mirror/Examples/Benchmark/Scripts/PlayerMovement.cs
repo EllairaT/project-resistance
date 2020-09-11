e939b544541f65f175e118617f6a923a6b8f3ddf
Scuftz <shivneel.singh1600@gmail.com>
2020-09-11 13:12:46 +1200

    WORKING ANIMATIONS
    - Animations are now visible to all other players
    - Player's camera now work properly upon instantiating the game
    - Player's are able to move around

--- Resistance/Assets/Mirror/Examples/Benchmark/Scripts/PlayerMovement.cs
+++ Resistance/Assets/Mirror/Examples/Benchmark/Scripts/PlayerMovement.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-namespace Mirror.Examples.OneK
-{
-    public class PlayerMovement : NetworkBehaviour
-    {
-        public float speed = 5;
+//namespace Mirror.Examples.OneK
+//{
+//    public class PlayerMovement : NetworkBehaviour
+//    {
+//        public float speed = 5;
 
-        void Update()
-        {
-            if (!isLocalPlayer) return;
+//        void Update()
+//        {
+//            if (!isLocalPlayer) return;
 
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
+//            float h = Input.GetAxis("Horizontal");
+//            float v = Input.GetAxis("Vertical");
 
-            Vector3 dir = new Vector3(h, 0, v);
-            transform.position += dir.normalized * (Time.deltaTime * speed);
-        }
-    }
-}
+//            Vector3 dir = new Vector3(h, 0, v);
+//            transform.position += dir.normalized * (Time.deltaTime * speed);
+//        }
+//    }
+//}
